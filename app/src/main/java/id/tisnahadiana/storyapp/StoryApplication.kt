7daedc85a0ecd9aa5937f29c28bff8ce99ad69c0
Tisna Hadiana <tisnahadiana@gmail.com>
2023-04-25 19:50:19 +0700

    Adding Color and Style theme

---
+++ app/src/main/java/id/tisnahadiana/storyapp/StoryApplication.kt
@@ -0,0 +1,7 @@
+package id.tisnahadiana.storyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StoryApplication : Application()
--- app/src/main/java/id/tisnahadiana/storyapp/ui/main/MainActivity.kt
+++ app/src/main/java/id/tisnahadiana/storyapp/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package id.tisnahadiana.storyapp
+package id.tisnahadiana.storyapp.ui.main
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import id.tisnahadiana.storyapp.R
 import id.tisnahadiana.storyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
