c1ae710d093adbfe089ae10df9d3f93950e31c73
Tisna Hadiana <tisnahadiana@gmail.com>
2023-04-26 11:21:01 +0700

    Fix bug in Logout

--- app/src/main/java/id/tisnahadiana/storyapp/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/id/tisnahadiana/storyapp/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,12 @@
 package id.tisnahadiana.storyapp.ui.settings
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import id.tisnahadiana.storyapp.data.local.datastore.LoginPreferences
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
+@HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val loginPreferences: LoginPreferences
 ): ViewModel() {
