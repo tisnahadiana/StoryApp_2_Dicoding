942980c081cfc60ce2ac07550d4c5e9251758345
Tisna Hadiana <tisnahadiana@gmail.com>
2023-04-26 16:55:07 +0700

    Before change LoginPreferences

--- app/src/main/java/id/tisnahadiana/storyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/id/tisnahadiana/storyapp/ui/home/HomeFragment.kt
@@ -6,12 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadState
 import androidx.paging.PagingData
