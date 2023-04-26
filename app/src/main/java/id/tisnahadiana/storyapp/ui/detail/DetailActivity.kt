2797edbec27fbad26b2da2244cb9ba3945314b79
Tisna Hadiana <tisnahadiana@gmail.com>
2023-04-26 14:57:27 +0700

    Finish Layout Detail and code

--- app/src/main/java/id/tisnahadiana/storyapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/tisnahadiana/storyapp/ui/detail/DetailActivity.kt
@@ -1,13 +1,40 @@
 package id.tisnahadiana.storyapp.ui.detail
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import id.tisnahadiana.storyapp.R
+import id.tisnahadiana.storyapp.data.local.room.StoryEntity
+import id.tisnahadiana.storyapp.databinding.ActivityDetailBinding
+import java.io.Serializable
 
 class DetailActivity : AppCompatActivity() {
+
+    private val binding: ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        val story = intent.serializable<StoryEntity>(EXTRA_DETAIL)!!
+        val name = story.name
+        val description = story.description
+        val imgUrl = story.photoUrl
+
+        supportActionBar?.title = name
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        binding.tvDetailDescription.text = description
+
+        Glide.with(this)
+            .load(imgUrl)
+            .into(binding.ivDetailPhoto)
     }
 
 
@@ -15,4 +42,9 @@ class DetailActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_DETAIL = "extra_detail"
     }
+}
+
+inline fun <reified T : Serializable> Intent.serializable(key: String): T? = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getSerializableExtra(key) as? T
 }
